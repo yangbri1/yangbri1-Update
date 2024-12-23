c758dd1f0f5876f2966b590e718d98a4c6bff5ea
Brian Yang <yangbri1@msu.edu>
2024-12-23 07:02:26 +0000

    Practicing with SQL DML UPDATE functionality to UPDATE an existing record's value by its column & IMPORTANT: Need to be referenced using the optional 'WHERE' clause otw will update all values in 'lastname' column

---
+++ problem1.sql
@@ -0,0 +1,11 @@
+UPDATE site_user SET lastname='Rush' WHERE id=2;
+-- UPDATE table_name SET col_1 = val_1, col_2 = val_2, ...col_N = val_N WHERE condition;
+/* Above is for DML UPDATE cmd syntax from 'UpdateARecord.java' */
+
+-- Update Alexa's last name to be 'Rush' in the site_user table.
+-- below way works too -- also earlier tests were NOT passing because had comments preceding UPDATE statement -- must've interfere w/ sample tests
+/*
+UPDATE site_user
+SET lastname='Rush'
+WHERE id=2;
+*/
